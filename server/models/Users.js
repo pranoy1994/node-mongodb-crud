9a9a8547ea35bf3d28b56742852e7bb4f24c8567
Pranoy Biswas <pranoybiswas1994@yahoo.com>
2017-05-16 07:58:27 +0530

    Added POST /todos route

---
+++ server/models/Users.js
@@ -0,0 +1,29 @@
+
+var mongoose = require('mongoose');
+
+var Users = mongoose.model('Users',{
+   
+    email:{
+        type:String,
+        minlength:1,
+        reqired:true,
+        trim:true
+    }
+    
+});
+
+module.exports = {Users};
+
+
+
+/*
+var newUser = new Users({
+    email:"  pranoybiswas1994@gmail.com"
+});*/
+
+/*
+newUser.save().then((res)=>{
+    console.log(res);
+},(err)=>{
+    console.log("Some error occured!");
+});*/
---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require("./db/mongoose");
+var {Todo} = require("./models/Todo");
+var {Users} = require("./models/Users");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+   console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc)=>{
+        res.send(doc);
+    },(err)=>{
+        res.status(400).send(err);
+    });
+});
+
+
+
+app.listen(3000, ()=>{
+   console.log("Server is up on port 3000"); 
+});
